96811c908e3bd3d2440a403a36c4d2b852d899f6
skyJinc <jincan0213@hotmail.com>
2015-07-16 10:36:35 +0800

    toolbar增加样式

--- app/src/main/java/j2w/team/view/J2WActivity.java
+++ app/src/main/java/j2w/team/view/J2WActivity.java
@@ -7,7 +7,6 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MotionEvent;
