410d9ccc1e3cdea182096ff41feea73950fe7af1
金灿 <jincan0213@hotmail.com>
2015-08-16 21:18:15 +0800

    通讯录详细信息

---
+++ app/src/main/java/j2w/team/common/utils/contact/ContactDetailModel.java
@@ -0,0 +1,108 @@
+package j2w.team.common.utils.contact;
+
+import java.util.List;
+
+import android.graphics.Bitmap;
+
+/**
+ * @创建人 sky
+ * @创建时间 15/5/4 下午5:33
+ * @类描述 联系人实体类
+ */
+public class ContactDetailModel implements Cloneable {
+
+	/**
+	 * 用户名称
+	 */
+	public String				name;
+
+	/**
+	 * 昵称
+	 */
+	public String				nickname;
+
+	/**
+	 * 公司
+	 */
+	public String				organization;
+
+	/**
+	 * 联网电话
+	 */
+	public String				networkPhone;
+
+	/**
+	 * 生日
+	 */
+	public String				birthday;
+
+	/**
+	 * 阴历生日
+	 */
+	public String				lunarBirthday;
+
+	/**
+	 * 备注
+	 */
+	public String				note;
+
+	/**
+	 * 数据库-联系人ID
+	 */
+	public String				contactId;
+
+	/**
+	 * 联系人头像
+	 */
+	public Bitmap				photo;
+
+	/**
+	 * 电子邮件, Key = type of email, Value = Email address
+	 */
+	public List<ContactEmail>	emailAddresses;
+
+	/**
+	 * 所有电话号码, Key = type of phone number, Value = phone numbers
+	 */
+	public List<ContactPhone>	phoneNumbers;
+
+	/**
+	 * IM
+	 */
+	public List<ContactIM>		contactIMs;
+
+	/**
+	 * 邮件
+	 */
+	public List<ContactEmail>	contactEmails;
+
+	/**
+	 * 地址
+	 */
+	public List<ContactAddress>	contactAddresses;
+
+	/**
+	 * 网址
+	 */
+	public List<ContactWebsite>	contactWebsites;
+
+	/**
+	 * 克隆
+	 *
+	 * @return
+	 */
+	public Object clone() {
+		try {
+			return super.clone();
+		} catch (CloneNotSupportedException e) {
+			return null;
+		}
+	}
+
+	@Override public String toString() {
+		return "ContactDetailModel{" + "name='" + name + '\'' + ", nickname='" + nickname + '\'' + ", organization='" + organization + '\'' + ", networkPhone='" + networkPhone + '\'' + ", birthday='"
+				+ birthday + '\'' + ", lunarBirthday='" + lunarBirthday + '\'' + ", note='" + note + '\'' + ", contactId='" + contactId + '\'' + ", photo=" + photo + ", emailAddresses="
+				+ emailAddresses + ", phoneNumbers=" + phoneNumbers + ", contactIMs=" + contactIMs + ", contactEmails=" + contactEmails + ", contactAddresses=" + contactAddresses
+				+ ", contactWebsites=" + contactWebsites + '}';
+	}
+}
