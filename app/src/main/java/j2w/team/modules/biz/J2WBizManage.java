e2206c8ebf2b1142988041754dea8f0c853bbb52
金灿 <jincan0213@hotmail.com>
2015-09-08 17:40:29 +0800

    修复

--- app/src/main/java/j2w/team/modules/biz/J2WBizManage.java
+++ app/src/main/java/j2w/team/modules/biz/J2WBizManage.java
@@ -3,6 +3,8 @@ package j2w.team.modules.biz;
 import java.util.HashMap;
 import java.util.Map;
 
+import j2w.team.biz.J2WIBiz;
+
 /**
  * @创建人 sky
  * @创建时间 15/9/8 下午4:59
@@ -16,5 +18,7 @@ public class J2WBizManage implements J2WBizIManage {
 		this.stackBiz = new HashMap<>();
 	}
 
-
+	@Override public <B extends J2WIBiz> B biz(Class<B> biz) {
+		return null;
+	}
 }
