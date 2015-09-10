c9046117713b3cdca9a39961acd1fdae794fb71c
金灿 <jincan0213@hotmail.com>
2015-09-10 12:27:18 +0800

    增加工具

--- app/src/main/java/j2w/team/common/utils/J2WGsonUtils.java
+++ app/src/main/java/j2w/team/common/utils/J2WGsonUtils.java
@@ -1,6 +1,12 @@
 package j2w.team.common.utils;
 
 import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
+import com.google.gson.TypeAdapterFactory;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
 import com.squareup.okhttp.ResponseBody;
 
 import java.io.BufferedReader;
@@ -48,6 +54,33 @@ public class J2WGsonUtils {
 
 			} catch (IOException ignored) {
 			}
+		}
+	}
+
+	public static class NullStringToEmptyAdapterFactory<T> implements TypeAdapterFactory {
+		public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type) {
+
+			Class<T> rawType = (Class<T>) type.getRawType();
+			if (rawType != String.class) {
+				return null;
+			}
+			return (TypeAdapter<T>) new StringAdapter();
+		}
+	}
+	private static class StringAdapter extends TypeAdapter<String> {
+		public String read(JsonReader reader) throws IOException {
+			if (reader.peek() == JsonToken.NULL) {
+				reader.nextNull();
+				return "";
+			}
+			return reader.nextString();
+		}
+		public void write(JsonWriter writer, String value) throws IOException {
+			if (value == null|| value.equals("")) {
+				writer.nullValue();
+				return;
+			}
+			writer.value(value);
 		}
 	}
 }
