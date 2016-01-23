54506154a602c124810aec64c0d28a6a9ba5ec0c
金灿 <jincan0213@hotmail.com>
2016-01-23 18:46:21 +0800

    修复

--- app/src/main/java/j2w/team/modules/toast/J2WToast.java
+++ app/src/main/java/j2w/team/modules/toast/J2WToast.java
@@ -8,8 +8,7 @@ import j2w.team.J2WApplication;
 import j2w.team.J2WHelper;
 
 /**
- * Created by wungko on 15/3/17. 弱交互Tost 消息弹窗
- * Update by skyJC on 15/8/05
+ * Created by wungko on 15/3/17. 弱交互Tost 消息弹窗 Update by skyJC on 15/8/05
  */
 public class J2WToast {
 
@@ -41,6 +40,27 @@ public class J2WToast {
 			});
 		} else {
 			showToast(msg, Toast.LENGTH_SHORT);
+		}
+	}
+
+	/**
+	 * 简单Toast 消息弹出
+	 * 
+	 * @param msg
+	 */
+	public void show(final int msg) {
+		// 判断是否在主线程
+		boolean isMainLooper = Looper.getMainLooper().getThread() != Thread.currentThread();
+
+		if (isMainLooper) {
+			J2WHelper.mainLooper().execute(new Runnable() {
+
+				@Override public void run() {
+					showToast(J2WHelper.getInstance().getString(msg), Toast.LENGTH_SHORT);
+				}
+			});
+		} else {
+			showToast(J2WHelper.getInstance().getString(msg), Toast.LENGTH_SHORT);
 		}
 	}
 
