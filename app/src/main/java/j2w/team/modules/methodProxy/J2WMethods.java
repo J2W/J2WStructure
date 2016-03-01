d1d4c840336318c7a4d94855da465159133b4587
金灿 <jincan0213@hotmail.com>
2016-03-01 17:53:23 +0800

    新增打印

--- app/src/main/java/j2w/team/modules/methodProxy/J2WMethods.java
+++ app/src/main/java/j2w/team/modules/methodProxy/J2WMethods.java
@@ -1,10 +1,15 @@
 package j2w.team.modules.methodProxy;
 
+import android.os.Build;
+import android.os.Looper;
+import android.os.Trace;
 import android.support.v4.util.SimpleArrayMap;
+import android.util.Log;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 import j2w.team.J2WHelper;
 import j2w.team.common.utils.J2WCheckUtils;
@@ -16,6 +21,7 @@ import j2w.team.core.plugin.J2WFragmentInterceptor;
 import j2w.team.core.plugin.J2WHttpErrorInterceptor;
 import j2w.team.core.plugin.ImplStartInterceptor;
 import j2w.team.core.plugin.BizStartInterceptor;
+import j2w.team.modules.log.L;
 
 /**
  * @创建人 sky
@@ -30,17 +36,19 @@ public final class J2WMethods {
 
 	final J2WFragmentInterceptor				j2WFragmentInterceptor;
 
-	final ArrayList<BizStartInterceptor> bizStartInterceptor;		// 方法开始拦截器
+	final ArrayList<BizStartInterceptor>		bizStartInterceptor;		// 方法开始拦截器
 
-	final ArrayList<BizEndInterceptor> bizEndInterceptor;			// 方法结束拦截器
+	final ArrayList<BizEndInterceptor>			bizEndInterceptor;			// 方法结束拦截器
 
-	private ArrayList<ImplStartInterceptor> implStartInterceptors;	// 方法开始拦截器
+	private ArrayList<ImplStartInterceptor>		implStartInterceptors;		// 方法开始拦截器
 
-	private ArrayList<ImplEndInterceptor> implEndInterceptors;	// 方法结束拦截器
+	private ArrayList<ImplEndInterceptor>		implEndInterceptors;		// 方法结束拦截器
 
 	final ArrayList<J2WErrorInterceptor>		j2WErrorInterceptor;		// 方法错误拦截器
 
 	final ArrayList<J2WHttpErrorInterceptor>	j2WHttpErrorInterceptor;	// 方法错误拦截器
+
+	private boolean								isOpenLog;
 
 	public J2WMethods(J2WActivityInterceptor j2WActivityInterceptor, J2WFragmentInterceptor j2WFragmentInterceptor, ArrayList<BizStartInterceptor> bizStartInterceptor,
 			ArrayList<BizEndInterceptor> bizEndInterceptor, ArrayList<ImplStartInterceptor> implStartInterceptors, ArrayList<ImplEndInterceptor> implEndInterceptors,
@@ -54,6 +62,7 @@ public final class J2WMethods {
 		this.j2WHttpErrorInterceptor = j2WHttpErrorInterceptor;
 		this.j2WActivityInterceptor = j2WActivityInterceptor;
 		this.j2WFragmentInterceptor = j2WFragmentInterceptor;
+		this.isOpenLog = J2WHelper.getInstance().isLogOpen();
 	}
 
 	/**
@@ -70,9 +79,66 @@ public final class J2WMethods {
 			@Override public Object invoke(Object proxy, Method method, Object... args) throws Throwable {
 				String key = getKey(impl, method, method.getParameterTypes());
 				J2WMethod j2WMethod = loadJ2WMethod(key, method, service);
-				return j2WMethod.invoke(impl, args);
+				// 开始
+				if(!isOpenLog){
+					return j2WMethod.invoke(impl, args);
+				}
+				enterMethod(method, args);
+				long startNanos = System.nanoTime();
+				Object result = j2WMethod.invoke(impl, args);
+				long stopNanos = System.nanoTime();
+				long lengthMillis = TimeUnit.NANOSECONDS.toMillis(stopNanos - startNanos);
+				exitMethod(method, result, lengthMillis);
+
+				return result;
 			}
 		});
+	}
+
+	private void enterMethod(Method method, Object... args) {
+		Class<?> cls = method.getDeclaringClass();
+		String methodName = method.getName();
+		Class[] parameterNames = method.getParameterTypes();
+		Object[] parameterValues = args;
+
+		StringBuilder builder = new StringBuilder("\u21E2 ");
+		builder.append(methodName).append('(');
+		for (int i = 0; i < parameterValues.length; i++) {
+			if (i > 0) {
+				builder.append(", ");
+			}
+			builder.append(parameterNames[i]).append('=');
+			builder.append(Strings.toString(parameterValues[i]));
+		}
+		builder.append(')');
+
+		if (Looper.myLooper() != Looper.getMainLooper()) {
+			builder.append(" [Thread:\"").append(Thread.currentThread().getName()).append("\"]");
+		}
+
+		Log.v(cls.getSimpleName(), builder.toString());
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+			final String section = builder.toString().substring(2);
+			Trace.beginSection(section);
+		}
+	}
+
+	private void exitMethod(Method method, Object result, long lengthMillis) {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+			Trace.endSection();
+		}
+		Class<?> cls = method.getDeclaringClass();
+		String methodName = method.getName();
+		boolean hasReturnType = method.getReturnType() != void.class;
+
+		StringBuilder builder = new StringBuilder("\u21E0 ").append(methodName).append(" [").append(lengthMillis).append("ms]");
+
+		if (hasReturnType) {
+			builder.append(" = ");
+			builder.append(Strings.toString(result));
+		}
+		Log.v(cls.getSimpleName(), builder.toString());
 	}
 
 	/**
@@ -91,7 +157,16 @@ public final class J2WMethods {
 				for (ImplStartInterceptor item : J2WHelper.methodsProxy().implStartInterceptors) {
 					item.interceptStart(impl.getClass().getName(), service, method, args);
 				}
+
+				if(!isOpenLog){
+					return method.invoke(impl, args);
+				}
+				enterMethod(method, args);
+				long startNanos = System.nanoTime();
 				Object backgroundResult = method.invoke(impl, args);// 执行
+				long stopNanos = System.nanoTime();
+				long lengthMillis = TimeUnit.NANOSECONDS.toMillis(stopNanos - startNanos);
+				exitMethod(method, backgroundResult, lengthMillis);
 				// 业务拦截器 - 后
 				for (ImplEndInterceptor item : J2WHelper.methodsProxy().implEndInterceptors) {
 					item.interceptEnd(impl.getClass().getName(), service, method, args, backgroundResult);
@@ -166,11 +241,11 @@ public final class J2WMethods {
 
 		private ArrayList<BizStartInterceptor>		j2WStartInterceptors;		// 方法开始拦截器
 
-		private ArrayList<BizEndInterceptor> bizEndInterceptors;		// 方法结束拦截器
+		private ArrayList<BizEndInterceptor>		bizEndInterceptors;		// 方法结束拦截器
 
-		private ArrayList<ImplStartInterceptor> implStartInterceptors;	// 方法开始拦截器
+		private ArrayList<ImplStartInterceptor>		implStartInterceptors;		// 方法开始拦截器
 
-		private ArrayList<ImplEndInterceptor> implEndInterceptors;	// 方法结束拦截器
+		private ArrayList<ImplEndInterceptor>		implEndInterceptors;		// 方法结束拦截器
 
 		private ArrayList<J2WErrorInterceptor>		j2WErrorInterceptors;		// 方法错误拦截器
 
