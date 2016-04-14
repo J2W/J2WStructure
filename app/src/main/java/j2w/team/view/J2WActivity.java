fcdd15598d947426e77f4f0b366b6b66741b88e8
金灿 <jincan0213@hotmail.com>
2016-04-14 14:21:19 +0800

    修复

--- app/src/main/java/j2w/team/view/J2WActivity.java
+++ app/src/main/java/j2w/team/view/J2WActivity.java
@@ -35,325 +35,342 @@ import j2w.team.view.adapter.recycleview.J2WRVAdapterItem;
  */
 public abstract class J2WActivity<B extends J2WIBiz> extends AppCompatActivity {
 
-	/**
-	 * 定制
-	 *
-	 * @param initialJ2WBuilder
-	 * @return
-	 **/
-	protected abstract J2WBuilder build(J2WBuilder initialJ2WBuilder);
+    /**
+     * 定制
+     *
+     * @param initialJ2WBuilder
+     * @return
+     **/
+    protected abstract J2WBuilder build(J2WBuilder initialJ2WBuilder);
 
-	/**
-	 * 初始化数据
-	 *
-	 * @param savedInstanceState
-	 *            数据
-	 */
-	protected abstract void initData(Bundle savedInstanceState);
+    /**
+     * 初始化数据
+     *
+     * @param savedInstanceState 数据
+     */
+    protected abstract void initData(Bundle savedInstanceState);
 
-	/**
-	 * View层编辑器
-	 **/
-	private J2WBuilder	j2WBuilder;
+    /**
+     * View层编辑器
+     **/
+    private J2WBuilder j2WBuilder;
 
-	J2WStructureModel	j2WStructureModel;
+    J2WStructureModel j2WStructureModel;
 
-	/**
-	 * 初始化
-	 *
-	 * @param savedInstanceState
-	 */
-	@Override protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		/** 初始化结构 **/
-		j2WStructureModel = new J2WStructureModel(this);
-		J2WHelper.structureHelper().attach(j2WStructureModel);
-		/** 初始化堆栈 **/
-		J2WHelper.screenHelper().onCreate(this);
-		/** 活动拦截器 **/
-		J2WHelper.methodsProxy().activityInterceptor().onCreate(this, getIntent().getExtras(), savedInstanceState);
-		/** 初始化视图 **/
-		LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		j2WBuilder = new J2WBuilder(this, inflater);
-		setContentView(build(j2WBuilder).create());
-		/** 初始化所有组建 **/
-		ButterKnife.bind(this);
-		/** 状态栏颜色 **/
-		j2WBuilder.initTint();
-		/** 初始化数据 **/
-		initData(getIntent().getExtras());
-	}
+    /**
+     * 初始化
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /** 初始化结构 **/
+        j2WStructureModel = new J2WStructureModel(this);
+        J2WHelper.structureHelper().attach(j2WStructureModel);
+        /** 初始化堆栈 **/
+        J2WHelper.screenHelper().onCreate(this);
+        /** 活动拦截器 **/
+        J2WHelper.methodsProxy().activityInterceptor().onCreate(this, getIntent().getExtras(), savedInstanceState);
+        /** 初始化视图 **/
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        j2WBuilder = new J2WBuilder(this, inflater);
+        setContentView(build(j2WBuilder).create());
+        /** 初始化所有组建 **/
+        ButterKnife.bind(this);
+        /** 状态栏颜色 **/
+        j2WBuilder.initTint();
+        /** 初始化数据 **/
+        initData(getIntent().getExtras());
+    }
 
-	@Override protected void onStart() {
-		super.onStart();
-		J2WHelper.methodsProxy().activityInterceptor().onStart(this);
-	}
+    @Override
+    protected void onStart() {
+        super.onStart();
+        J2WHelper.methodsProxy().activityInterceptor().onStart(this);
+    }
 
-	@Override protected void onResume() {
-		super.onResume();
-		J2WHelper.screenHelper().onResume(this);
-		J2WHelper.methodsProxy().activityInterceptor().onResume(this);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        J2WHelper.screenHelper().onResume(this);
+        J2WHelper.methodsProxy().activityInterceptor().onResume(this);
 
-		/** 判断EventBus 是否注册 **/
-		if (j2WBuilder.isOpenEventBus()) {
-			if (!J2WHelper.eventBus().isRegistered(this)) {
-				J2WHelper.eventBus().register(this);
-			}
-		}
-		listLoadMoreOpen();
-	}
+        /** 判断EventBus 是否注册 **/
+        if (j2WBuilder.isOpenEventBus()) {
+            if (!J2WHelper.eventBus().isRegistered(this)) {
+                J2WHelper.eventBus().register(this);
+            }
+        }
+        listLoadMoreOpen();
+    }
 
-	@Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		J2WHelper.screenHelper().onActivityResult(this);
-	}
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        J2WHelper.screenHelper().onActivityResult(this);
+    }
 
-	/**
-	 * 设置输入法
-	 *
-	 * @param mode
-	 */
-	public void setSoftInputMode(int mode) {
-		getWindow().setSoftInputMode(mode);
-	}
+    /**
+     * 设置输入法
+     *
+     * @param mode
+     */
+    public void setSoftInputMode(int mode) {
+        getWindow().setSoftInputMode(mode);
+    }
 
-	@Override protected void onPause() {
-		super.onPause();
-		J2WHelper.screenHelper().onPause(this);
-		J2WHelper.methodsProxy().activityInterceptor().onPause(this);
-		/** 判断EventBus 是否销毁 **/
-		if (j2WBuilder.isOpenEventBus()) {
-			if (!j2WBuilder.isNotCloseEventBus()) {
-				if (J2WHelper.eventBus().isRegistered(this)) {
-					J2WHelper.eventBus().unregister(this);
-				}
-			}
-		}
-		// 恢复初始化
-		listRefreshing(false);
-	}
+    @Override
+    protected void onPause() {
+        super.onPause();
+        J2WHelper.screenHelper().onPause(this);
+        J2WHelper.methodsProxy().activityInterceptor().onPause(this);
+        /** 判断EventBus 是否销毁 **/
+        if (j2WBuilder.isOpenEventBus()) {
+            if (!j2WBuilder.isNotCloseEventBus()) {
+                if (J2WHelper.eventBus().isRegistered(this)) {
+                    J2WHelper.eventBus().unregister(this);
+                }
+            }
+        }
+        // 恢复初始化
+        listRefreshing(false);
+    }
 
-	@Override protected void onRestart() {
-		super.onRestart();
-		J2WHelper.methodsProxy().activityInterceptor().onRestart(this);
-	}
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        J2WHelper.methodsProxy().activityInterceptor().onRestart(this);
+    }
 
-	@Override protected void onStop() {
-		super.onStop();
-		J2WHelper.methodsProxy().activityInterceptor().onStop(this);
-	}
+    @Override
+    protected void onStop() {
+        super.onStop();
+        J2WHelper.methodsProxy().activityInterceptor().onStop(this);
+    }
 
-	@Override protected void onDestroy() {
-		super.onDestroy();
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        detach();
+        /** 关闭event **/
+        if (J2WHelper.eventBus().isRegistered(this)) {
+            J2WHelper.eventBus().unregister(this);
+        }
+        /** 移除builder **/
+        j2WBuilder.detach();
+        j2WBuilder = null;
+        /** 清空注解view **/
+        ButterKnife.unbind(this);
+        J2WHelper.structureHelper().detach(j2WStructureModel);
+        J2WHelper.screenHelper().onDestroy(this);
+        J2WHelper.methodsProxy().activityInterceptor().onDestroy(this);
+        /** 关闭键盘 **/
+        J2WKeyboardUtils.hideSoftInput(this);
+    }
 
-		/** 关闭event **/
-		if (J2WHelper.eventBus().isRegistered(this)) {
-			J2WHelper.eventBus().unregister(this);
-		}
-		/** 移除builder **/
-		j2WBuilder.detach();
-		j2WBuilder = null;
-		/** 清空注解view **/
-		ButterKnife.unbind(this);
-		J2WHelper.structureHelper().detach(j2WStructureModel);
-		J2WHelper.screenHelper().onDestroy(this);
-		J2WHelper.methodsProxy().activityInterceptor().onDestroy(this);
-		/** 关闭键盘 **/
-		J2WKeyboardUtils.hideSoftInput(this);
-	}
+    /**
+     * 清空
+     */
+    protected void detach() {
+    }
 
-	public void setLanding() {
-		J2WHelper.screenHelper().setAsLanding(this);
-	}
+    public void setLanding() {
+        J2WHelper.screenHelper().setAsLanding(this);
+    }
 
-	@Override public boolean onOptionsItemSelected(MenuItem item) {
-		if (item.getItemId() == android.R.id.home) {
-			onBackPressed();
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-	/**
-	 * 创建menu
-	 *
-	 * @param menu
-	 * @return
-	 */
-	@Override public boolean onCreateOptionsMenu(Menu menu) {
-		if (j2WBuilder != null && j2WBuilder.getToolbarMenuId() > 0) {
-			getMenuInflater().inflate(j2WBuilder.getToolbarMenuId(), menu);
-		}
-		return super.onCreateOptionsMenu(menu);
-	}
+    /**
+     * 创建menu
+     *
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (j2WBuilder != null && j2WBuilder.getToolbarMenuId() > 0) {
+            getMenuInflater().inflate(j2WBuilder.getToolbarMenuId(), menu);
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
 
-	public <D extends J2WIDisplay> D display(Class<D> eClass) {
-		return j2WStructureModel.display(eClass);
-	}
+    public <D extends J2WIDisplay> D display(Class<D> eClass) {
+        return j2WStructureModel.display(eClass);
+    }
 
-	public B biz() {
-		return (B) j2WStructureModel.getJ2WProxy().proxy;
-	}
+    public B biz() {
+        return (B) j2WStructureModel.getJ2WProxy().proxy;
+    }
 
-	public <C extends J2WIBiz> C biz(Class<C> service) {
-		if (j2WStructureModel.getService().equals(service)) {
-			return (C) j2WStructureModel.getJ2WProxy().proxy;
-		}
-		return J2WHelper.structureHelper().biz(service);
-	}
+    public <C extends J2WIBiz> C biz(Class<C> service) {
+        if (j2WStructureModel.getService().equals(service)) {
+            return (C) j2WStructureModel.getJ2WProxy().proxy;
+        }
+        return J2WHelper.structureHelper().biz(service);
+    }
 
-	@Override public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if (J2WHelper.structureHelper().onKeyBack(keyCode, getSupportFragmentManager(), this)) {
-			return true;
-		}
-		return super.onKeyDown(keyCode, event);
-	}
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (J2WHelper.structureHelper().onKeyBack(keyCode, getSupportFragmentManager(), this)) {
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 
-	/**********************
-	 * View业务代码
-	 *********************/
+    /**********************
+     * View业务代码
+     *********************/
 
-	public <T> T findFragment(Class<T> clazz) {
-		J2WCheckUtils.checkNotNull(clazz, "class不能为空");
-		return (T) getSupportFragmentManager().findFragmentByTag(clazz.getName());
-	}
+    public <T> T findFragment(Class<T> clazz) {
+        J2WCheckUtils.checkNotNull(clazz, "class不能为空");
+        return (T) getSupportFragmentManager().findFragmentByTag(clazz.getName());
+    }
 
-	public J2WView j2wView() {
-		return j2WBuilder.getJ2WView();
-	}
+    public J2WView j2wView() {
+        return j2WBuilder.getJ2WView();
+    }
 
-	public boolean isOpenTint() {
-		return j2WBuilder.isTintColor();
-	}
+    public boolean isOpenTint() {
+        return j2WBuilder.isTintColor();
+    }
 
-	/**********************
-	 * Actionbar业务代码
-	 *********************/
+    /**********************
+     * Actionbar业务代码
+     *********************/
 
-	protected void showContent() {
-		if (j2WBuilder != null) {
-			j2WBuilder.layoutContent();
-		}
-	}
+    protected void showContent() {
+        if (j2WBuilder != null) {
+            j2WBuilder.layoutContent();
+        }
+    }
 
-	protected void showLoading() {
-		if (j2WBuilder != null) {
-			j2WBuilder.layoutLoading();
-		}
-	}
+    protected void showLoading() {
+        if (j2WBuilder != null) {
+            j2WBuilder.layoutLoading();
+        }
+    }
 
-	protected void showBizError() {
-		if (j2WBuilder != null) {
-			j2WBuilder.layoutBizError();
-		}
-	}
+    protected void showBizError() {
+        if (j2WBuilder != null) {
+            j2WBuilder.layoutBizError();
+        }
+    }
 
-	protected void showEmpty() {
-		if (j2WBuilder != null) {
-			j2WBuilder.layoutEmpty();
-		}
-	}
+    protected void showEmpty() {
+        if (j2WBuilder != null) {
+            j2WBuilder.layoutEmpty();
+        }
+    }
 
-	protected void showHttpError() {
-		if (j2WBuilder != null) {
-			j2WBuilder.layoutHttpError();
-		}
-	}
+    protected void showHttpError() {
+        if (j2WBuilder != null) {
+            j2WBuilder.layoutHttpError();
+        }
+    }
 
-	/**********************
-	 * Actionbar业务代码
-	 *********************/
-	public Toolbar toolbar() {
-		return j2WBuilder.getToolbar();
-	}
+    /**********************
+     * Actionbar业务代码
+     *********************/
+    public Toolbar toolbar() {
+        return j2WBuilder.getToolbar();
+    }
 
-	public SystemBarTintManager tintManager() {
-		return j2WBuilder.getTintManager();
-	}
+    public SystemBarTintManager tintManager() {
+        return j2WBuilder.getTintManager();
+    }
 
-	/**********************
-	 * RecyclerView业务代码
-	 *********************/
+    /**********************
+     * RecyclerView业务代码
+     *********************/
 
-	@Deprecated protected J2WRVAdapterItem adapterRecycler() {
-		return j2WBuilder.getJ2WRVAdapterItem();
-	}
+    @Deprecated
+    protected J2WRVAdapterItem adapterRecycler() {
+        return j2WBuilder.getJ2WRVAdapterItem();
+    }
 
-	protected J2WRVAdapter recyclerAdapter() {
-		return j2WBuilder.getJ2WRVAdapterItem2();
-	}
+    protected J2WRVAdapter recyclerAdapter() {
+        return j2WBuilder.getJ2WRVAdapterItem2();
+    }
 
-	protected RecyclerView.LayoutManager recyclerLayoutManager() {
-		return j2WBuilder.getLayoutManager();
-	}
+    protected RecyclerView.LayoutManager recyclerLayoutManager() {
+        return j2WBuilder.getLayoutManager();
+    }
 
-	protected RecyclerView recyclerView() {
-		return j2WBuilder.getRecyclerView();
-	}
+    protected RecyclerView recyclerView() {
+        return j2WBuilder.getRecyclerView();
+    }
 
-	/**********************
-	 * ListView业务代码
-	 *********************/
+    /**********************
+     * ListView业务代码
+     *********************/
 
-	protected void addListHeader() {
-		if (j2WBuilder != null) {
-			j2WBuilder.addListHeader();
-		}
-	}
+    protected void addListHeader() {
+        if (j2WBuilder != null) {
+            j2WBuilder.addListHeader();
+        }
+    }
 
-	protected void addListFooter() {
-		if (j2WBuilder != null) {
-			j2WBuilder.addListFooter();
-		}
-	}
+    protected void addListFooter() {
+        if (j2WBuilder != null) {
+            j2WBuilder.addListFooter();
+        }
+    }
 
-	protected void removeListHeader() {
-		if (j2WBuilder != null) {
-			j2WBuilder.removeListHeader();
-		}
-	}
+    protected void removeListHeader() {
+        if (j2WBuilder != null) {
+            j2WBuilder.removeListHeader();
+        }
+    }
 
-	protected void removeListFooter() {
-		if (j2WBuilder != null) {
-			j2WBuilder.removeListFooter();
-		}
+    protected void removeListFooter() {
+        if (j2WBuilder != null) {
+            j2WBuilder.removeListFooter();
+        }
 
-	}
+    }
 
-	protected void listRefreshing(boolean bool) {
-		if (j2WBuilder != null) {
-			j2WBuilder.listRefreshing(bool);
-		}
-	}
+    protected void listRefreshing(boolean bool) {
+        if (j2WBuilder != null) {
+            j2WBuilder.listRefreshing(bool);
+        }
+    }
 
-	protected void listLoadMoreOpen() {
-		if (j2WBuilder != null) {
-			j2WBuilder.loadMoreOpen();
-		}
-	}
+    protected void listLoadMoreOpen() {
+        if (j2WBuilder != null) {
+            j2WBuilder.loadMoreOpen();
+        }
+    }
 
-	protected J2WListAdapter adapter() {
-		return j2WBuilder.getAdapter();
-	}
+    protected J2WListAdapter adapter() {
+        return j2WBuilder.getAdapter();
+    }
 
-	protected ListView listView() {
-		return j2WBuilder.getListView();
-	}
+    protected ListView listView() {
+        return j2WBuilder.getListView();
+    }
 
-	/**********************
-	 * ViewPager业务代码
-	 *********************/
+    /**********************
+     * ViewPager业务代码
+     *********************/
 
-	protected J2WIViewPagerAdapter viewPagerAdapter() {
-		return j2WBuilder.getViewPagerAdapter();
-	}
+    protected J2WIViewPagerAdapter viewPagerAdapter() {
+        return j2WBuilder.getViewPagerAdapter();
+    }
 
-	protected J2WViewPager viewPager() {
-		return j2WBuilder.getViewPager();
-	}
+    protected J2WViewPager viewPager() {
+        return j2WBuilder.getViewPager();
+    }
 
-	public boolean onKeyBack() {
-		onBackPressed();
-		return true;
-	}
+    public boolean onKeyBack() {
+        onBackPressed();
+        return true;
+    }
 }
